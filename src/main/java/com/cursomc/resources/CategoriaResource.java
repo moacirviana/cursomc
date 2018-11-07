7e1a6eb1a799167ad2ef3a1874f08a07b338fb3a
Moacir Viana <moacir.viana@gmail.com>
2018-11-07 14:55:35 -0400

    Deletando uma categoria

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -40,6 +40,12 @@ public class CategoriaResource {
 		   obj.setId(id);
 		   obj = service.update(obj);
 		   return ResponseEntity.noContent().build();
-	   }
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> update(@PathVariable Integer id){
+		   service.delete(id);
+		   return ResponseEntity.noContent().build();
+	}
 	
 }
