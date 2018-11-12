a963da487c1da4e9120d44075c862f2b2695b991
Moacir Viana <moacir.viana@gmail.com>
2018-11-12 15:08:23 -0400

    implementando envio de email pelo google

--- src/main/java/com/cursomc/config/DevConfig.java
+++ src/main/java/com/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.cursomc.services.DBService;
+import com.cursomc.services.EmailService;
+import com.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,10 +30,10 @@ public class DevConfig {
 		return true;
 	}
 
-	/*
+	
 	@Bean
 	public EmailService emailService() {
-		return new MockEmailService();
+		return new SmtpEmailService();
 	}
-	*/
+	
 }
