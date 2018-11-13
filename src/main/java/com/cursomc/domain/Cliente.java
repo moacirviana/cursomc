6c8f59681a4c06b586406f37ebc016e99049ebce
Moacir Viana <moacir.viana@gmail.com>
2018-11-13 09:34:42 -0400

    adicionando senha a classe Cliente

--- src/main/java/com/cursomc/domain/Cliente.java
+++ src/main/java/com/cursomc/domain/Cliente.java
@@ -35,6 +35,9 @@ public class Cliente implements Serializable{
   private Integer tipo;
   
   @JsonIgnore
+  private String senha;
+  
+  @JsonIgnore
   @OneToMany(mappedBy="cliente")
   private List<Pedido> pedidos = new ArrayList<>();
     
@@ -49,13 +52,14 @@ public class Cliente implements Serializable{
   public Cliente() {
   }
 
-  public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+  public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
 	super();
 	this.id = id;
 	this.nome = nome;
 	this.email = email;
 	this.cpfOuCnpj = cpfOuCnpj;
 	this.tipo = (tipo==null) ? null : tipo.getCod();
+	this.senha = senha;
   }
 
 	public Integer getId() {
@@ -122,6 +126,14 @@ public class Cliente implements Serializable{
 		this.pedidos = pedidos;
 	}
 	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -146,7 +158,6 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-
   
  
 }
--- src/main/java/com/cursomc/services/DBService.java
+++ src/main/java/com/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.cursomc.domain.Categoria;
@@ -32,9 +33,8 @@ import com.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
-	// @Autowired
-	// private BCryptPasswordEncoder pe;
-
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
@@ -202,13 +202,12 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "moacir.viana@gmail.com", "36378912377",
-				TipoCliente.PESSOAFISICA // , pe.encode("123")
+				TipoCliente.PESSOAFISICA , pe.encode("123")
 		);
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
-		Cliente cli2 = new Cliente(null, "Ana Costa", "moacir.viana@gmail.com", "31628382740", TipoCliente.PESSOAFISICA // ,
-																														// pe.encode("123")
+		Cliente cli2 = new Cliente(null, "Ana Costa", "moacir.viana@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123")
 		);
 		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 
